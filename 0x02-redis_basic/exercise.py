4f327e0dc9e59c7f04b7148c0df91c60e98fbf1a
M1urray <rknjonjo@gmail.com>
2023-04-18 09:13:10 +0300

    document get_int fucntion

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -79,6 +79,8 @@ class Cache():
         return value if not fn else fn(value)
 
     def get_int(self, key):
+        '''define the get int function
+        '''
         return self.get(key, int)
 
     def get_str(self, key):
